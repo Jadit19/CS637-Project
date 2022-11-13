34bf2829bb5a8fb1969cef9bfc11ab0c19361f1d
Jadit19 <jadit19@gmail.com>
2022-11-13 22:27:52 +0530

    parameterize

--- src/controller/src/controller.cpp
+++ src/controller/src/controller.cpp
@@ -6,18 +6,16 @@
 #include <mav_msgs/RollPitchYawrateThrust.h>
 #include <q_learning/data.h>
 
-int RATE = 30;
-double dt = 1.0 / double(RATE);
+// Paramas
+int RATE;
+double gravity;
+double fireflyMass;
+double targetAltitude;
 
-double kp = 0;
-double ki = 0;
-double kd = 0;
-double fireflyMass = 1.5;
-double gravity = 9.8;
-
-double altitude;
-double targetAltitude = 2;
-std::vector<double> angularVel(6);
+double dt;
+double kp;
+double ki;
+double kd;
 
 double prevErr = targetAltitude;
 double integralErr = 0;
@@ -29,7 +27,7 @@ void noThrust(){
     thrust.thrust.z = 0;
 }
 
-void setThrust(){
+void setThrust(double altitude){
     double err = targetAltitude - altitude;
     integralErr += err * dt;
     derivativeErr = (err-prevErr) / dt;
@@ -52,14 +50,19 @@ void gainsCallback(const q_learning::dataConstPtr& msg){
 }
 
 void poseCallback(const geometry_msgs::PoseConstPtr& msg){
-    altitude = msg->position.z;
-    setThrust();
+    setThrust(msg->position.z);
     return;
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "controller");
     ros::NodeHandle nh;
+
+    if (!nh.getParam("rate", RATE) || !nh.getParam("mass", fireflyMass) || !nh.getParam("gravity", gravity) || !nh.getParam("altitude", targetAltitude)){
+        ROS_ERROR("Couldn't load params!");
+        return 1;
+    }
+    dt = 1.0 / double(RATE);
 
     ros::Subscriber sub_gains_ = nh.subscribe<q_learning::data>("/learner", RATE, gainsCallback);
     ros::Subscriber sub_pose_ = nh.subscribe<geometry_msgs::Pose>("/firefly/ground_truth/pose", RATE, poseCallback);
--- src/q_learning/src/learner.cpp
+++ src/q_learning/src/learner.cpp
@@ -3,20 +3,25 @@
 
 #include <q_learning/data.h>
 
-int RATE = 30;
+int RATE;
 q_learning::data msg;
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "learner");
     ros::NodeHandle nh;
 
+    if (!nh.getParam("rate", RATE)){
+        ROS_ERROR("Couldn't load params!");
+        return 1;
+    }
+
     ros::Publisher pub_gains_ = nh.advertise<q_learning::data>("/learner", RATE);
     ros::Rate loopRate(RATE);
 
     msg.message.data = "Parameters post tuning";
-    msg.kp = 49.9;
-    msg.ki = 0;
-    msg.kd = 0;
+    msg.kp = 5;
+    msg.ki = 0.1;
+    msg.kd = 10;
 
     while (ros::ok()){
         ros::spinOnce();
